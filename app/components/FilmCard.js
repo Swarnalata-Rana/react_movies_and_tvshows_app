5ed89c20530442a0796b3eb8842d84f6c2d47fb8
Swarnalata Rana <swarnalatarana21@navgurukul.org>
2025-03-10 02:44:52 +0530

    AvgCount

--- app/components/FilmCard.js
+++ app/components/FilmCard.js
@@ -1,31 +1,53 @@
-"use client"
-import React, { useContext, useState } from "react";
-import Rating from "./Rating";
-import { FilmContext } from "../context/FilmContext";
-import FilmDetail from "./FilmDetail";
+import React, { useContext, useState } from 'react';
+import Rating from './Rating';
+import FilmDetail from './FilmDetail';
+import { FilmContext } from '../context/FilmContext';
 
 const FilmCard = ({ film }) => {
     const { fetchFilmDetails } = useContext(FilmContext);
     const [showDetailModal, setShowDetailModal] = useState(false);
     const [showRatingModal, setShowRatingModal] = useState(false);
+    const [ratings, setRatings] = useState([]);
+    const [avgRating, setAvgRating] = useState(0);
 
-    // DetailModal
+    function calculateAvgRating(newRatings) {
+        let sum = 0;
+        let i = 0;
+
+        while (i < newRatings.length) {
+            sum += newRatings[i];
+            i++;
+        }
+
+        return sum / newRatings.length;
+    }
+
+    function handleNewRating(newRating) {
+        const updatedRatings = [...ratings, newRating];
+        setRatings(updatedRatings);
+
+        const newAvgRating = calculateAvgRating(updatedRatings);
+        setAvgRating(newAvgRating.toFixed(1));
+    }
+
+
+    // DetalisModal
     const handleDetailClick = () => {
         fetchFilmDetails(film.imdbID);
         setShowDetailModal(true);
     };
+
     const handleCloseDetailModal = () => {
         setShowDetailModal(false);
     };
-
     // RatingModal
     const handleRatingClick = () => {
-        setShowRatingModal(true)
-    }
+        setShowRatingModal(true);
+    };
 
     const handleCloseRatingModal = () => {
-        setShowRatingModal(false)
-    }
+        setShowRatingModal(false);
+    };
 
     return (
         <>
@@ -35,21 +57,31 @@ const FilmCard = ({ film }) => {
                     <p>{film.Title}</p>
                     <p>{film.Year}</p>
                 </div>
-                <div className='avg'>Avg:{ } /5</div>
+                <div className="avg">Avg: {avgRating} / 5</div>
 
                 <div className="details-rating">
-                    <button className="rating-btn" onClick={handleRatingClick}>Rating</button>
-                    <button className="detailing-btn" onClick={handleDetailClick}>Details</button>
+                    <button className="rating-btn" onClick={handleRatingClick}>
+                        Rate
+                    </button>
+                    <button className="detailing-btn" onClick={handleDetailClick}>
+                        Details
+                    </button>
                 </div>
             </div>
+
             {showDetailModal && <FilmDetail onCloseD={handleCloseDetailModal} />}
-            {showRatingModal && <Rating onCloseR={handleCloseRatingModal} props={film} />}
 
+            {showRatingModal && (
+                <Rating
+                    onCloseR={handleCloseRatingModal}
+                    FilmTitle={film}
+                    updateAvgRating={handleNewRating}
+                    averageRating={avgRating}
+                />
+            )}
         </>
-    )
-}
+    );
+};
+
 export default FilmCard;
-
-
-
 
